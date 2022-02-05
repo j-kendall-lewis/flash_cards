6b2116b73346b7eadea16ce1e325315de7a4a1cd
j-kendall-lewis <jkendalllewis@gmail.com>
2022-02-04 17:10:36 -0700

    adding untested deck functionality

---
+++ lib/deck.rb
@@ -0,0 +1,9 @@
+class Deck
+    card_0= card.new("Juneau", "What is the capital of Alaska?", :Geography)
+    card_1= card.new("a code test", "What can I not write yet?", :Ruby)
+    card_2= card.new("my mind", "what have you lost recently?", :Self)
+    def initialize (deck_0)
+    deck_0 = Deck.new[card_0,card_1,card_2]
+    end
+    def 
+end
